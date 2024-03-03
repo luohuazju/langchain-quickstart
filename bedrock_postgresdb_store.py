26dd0d6c2c014de7f2fc8810c4e6ca61a9cdc94c
Carl <luohuazju@gmail.com>
2024-03-02 19:57:31 -0600

    bedrock to postgres

--- bedrock_postgresdb_store.py
+++ bedrock_postgresdb_store.py
@@ -5,6 +5,10 @@ import json
 import boto3
 import settings
 from langchain_community.embeddings import BedrockEmbeddings
+from langchain.docstore.document import Document
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores.pgvector import PGVector
 
 
 basic_header = HTTPBasicAuth(settings.CONSUL_USERNAME, settings.CONSUL_PASSWORD)
@@ -34,3 +38,34 @@ embeddings = BedrockEmbeddings(
 response = embeddings.embed_query("I am using Bedrock for embeddings as RAG for our GenAI")
 print(len(response),type(response))
 
+loader = TextLoader("./state_of_the_union.txt")
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+docs = text_splitter.split_documents(documents)
+
+CONNECTION_STRING = PGVector.connection_string_from_db_params(
+     driver="psycopg2",
+     host=settings.POSTGRESDB_HOST,
+     port=settings.POSTGRESDB_PORT,
+     database=settings.POSTGRESDB_NAME,
+     user=settings.POSTGRESDB_USERNAME,
+     password=settings.POSTGRESDB_PASSWORD,
+)
+
+COLLECTION_NAME = "state_of_the_union_test_titan_bedrock_python"
+
+db = PGVector.from_documents(
+    embedding=embeddings,
+    documents=docs,
+    collection_name=COLLECTION_NAME,
+    connection_string=CONNECTION_STRING,
+)
+
+query = "What did the president say about Ketanji Brown Jackson"
+docs_with_score = db.similarity_search_with_score(query)
+
+for doc, score in docs_with_score:
+    print("-" * 80)
+    print("Score: ", score)
+    print(doc.page_content)
+    print("-" * 80)
