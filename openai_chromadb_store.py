435231ff372508405686b4ffed0a8200e1d82f7c
Carl <luohuazju@gmail.com>
2024-07-08 19:29:50 -0500

    a working copy for chroma

--- openai_chromadb_store.py
+++ openai_chromadb_store.py
@@ -4,6 +4,10 @@ from langchain_openai import OpenAIEmbeddings
 import os
 import settings
 from langchain.text_splitter import TokenTextSplitter
+from langchain_openai import ChatOpenAI
+from langchain.chains import (
+    StuffDocumentsChain, ConversationalRetrievalChain
+)
 
 # chroma persist directory, otherwise it will be in memory
 persist_directory = "/Users/carl/work/genai/langchain-quickstart/public"
@@ -18,5 +22,19 @@ embeddings = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_API_KEY"])
 text_splitter = TokenTextSplitter(chunk_size=1000, chunk_overlap=0)
 doc_texts = text_splitter.split_documents(docs)
 vectordb = Chroma.from_documents(doc_texts, embeddings, persist_directory=persist_directory)
-vectordb.persist()
+# vectordb.persist()
 
+question = "What did the president say about Ketanji Brown Jackson"
+vectordb = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
+docs = vectordb.similarity_search(question, k=4)
+print(docs)
+
+# vectordb = Chroma.from_documents(docs, embeddings)
+retriever = vectordb.as_retriever()
+chain = ConversationalRetrievalChain.from_llm(
+    ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo"),
+    retriever=retriever,
+    return_source_documents=True)
+chat_history = []
+result = chain({"question": question, "chat_history": chat_history})
+print(result)
